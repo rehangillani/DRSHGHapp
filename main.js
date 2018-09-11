05f3f7ed4304a02bb5ef5d71bc79527b5e52face
Rehan Gillani <rehangillani.edu@gmail.com>
2018-09-11 10:56:00 +0500

    added main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    if($(window).height() == 480) {
+       $("span").removeClass("fa-3x lg-5s"); 
+
+    } 
+    if($(window).height() == 568) {
+       $("span").removeClass("logo-cent-small"); 
+
+    } 
+    if($(window).height() == ) {
+       $("span").removeClass("logo-cent"); 
+
+    } 
+});
